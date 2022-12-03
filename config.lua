f5b99e8fd7536371633ccf763f53eb14bc388b4d
kokardy <mgc1028@gmail.com>
2022-12-03 15:28:44 +0900

    comment out deprecated nofity property

--- config.lua
+++ config.lua
@@ -9,7 +9,7 @@ an executable
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 
 -- general
-lvim.log.level = "debug"
+lvim.log.level = "warning"
 lvim.format_on_save = true
 lvim.colorscheme = "industry"
 -- to disable icons and use a minimalist setup, uncomment the following
@@ -70,7 +70,7 @@ lvim.builtin.which_key.mappings["y"] = {
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
+-- lvim.builtin.notify.active = true -- Deprecated
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
@@ -189,6 +189,7 @@ lvim.plugins = {
   { "mattn/vim-maketable" },
   { "tyru/open-browser.vim" },
   { "weirongxu/plantuml-previewer.vim" },
+  { "aklt/plantuml-syntax" },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
