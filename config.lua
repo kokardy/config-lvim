df135bba1c0bc6457b5acea28848944bf7758c7c
kokardy <mgc1028@gmail.com>
2022-12-10 17:17:43 +0900

    add Oct(github plugin)

--- config.lua
+++ config.lua
@@ -208,7 +208,19 @@ lvim.plugins = {
   { "aklt/plantuml-syntax" },
 
   -- csv
-  { "mechatroner/rainbow_csv" }
+  { "mechatroner/rainbow_csv" },
+
+  -- github
+  { "pwntester/octo.nvim",
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'kyazdani42/nvim-web-devicons',
+    },
+    config = function()
+      require("octo").setup()
+    end,
+  },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
