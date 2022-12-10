df47d0ffc4f8e71f752a03ef98f1fa7669a71fb8
kokardy <mgc1028@gmail.com>
2022-12-10 17:24:45 +0900

    add plugin for treesitter

--- config.lua
+++ config.lua
@@ -221,6 +221,29 @@ lvim.plugins = {
       require("octo").setup()
     end,
   },
+
+  -- treesitter
+  { -- 関数名を表示
+    "romgrk/nvim-treesitter-context",
+    config = function()
+      require("treesitter-context").setup {
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        throttle = true, -- Throttles plugin updates (may improve performance)
+        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+        patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+          -- For all filetypes
+          -- Note that setting an entry here replaces all other patterns for this entry.
+          -- By setting the 'default' entry below, you can control which nodes you want to
+          -- appear in the context window.
+          default = {
+            'class',
+            'function',
+            'method',
+          },
+        },
+      }
+    end
+  },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
